12f977739971e58fbd784ab9158906eae95ff3d5
kaveesha-dilshan <kaveeshadilshan5445@gmail.com>
2026-09-16 19:43:41 +0530

    add login part to authcontroller

--- backend/server/controllers/authController.js
+++ backend/server/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import bcrypt, { hash } from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
     try {
@@ -37,6 +38,64 @@ export const registerUser = async (req, res) => {
             }
         })
 
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+// USER LOGIN
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password ){
+            return res.status(400).json({
+                message: "Email and Password required"
+            })
+        }
+
+        const user = await User.findOne({ email })
+
+        if (!user) {
+            return res.status(401).json({
+                message: "Invalid email or user does not exist"
+            })
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if (!isPasswordCorrect) {
+            return res.status(401).json({
+                message: "Invalid password"
+            })
+        }
+
+        const token = jwt.sign(
+            {
+                id: user._id,
+                role: user.role
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d"
+            }
+        )
+
+        res.status(200).json({
+            message: "Login successful",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            }
+        })
     } catch (error) {
         res.status(500).json({
             message: error.message
